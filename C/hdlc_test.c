a3d1526695c814c267de3636b13adbfc2937de3b
Luuk van Dijk <lvdlvd@users.noreply.github.com>
2023-01-05 11:25:51 +0100

    Comment hdlc_test better
    to explain what it does.

--- C/hdlc_test.c
+++ C/hdlc_test.c
@@ -9,15 +9,18 @@ int main(int argc, char *argv[]) {
 	uint8_t bufxmit[800];
 	uint8_t bufrecv[801]; // need 1 extra for closing flag
 
+	// Fill bufxmit with random garbage
 	for (int i = 0; i < 800; ++i) {
 		bufxmit[i] = lrand48();
 	}
 
+	// hdlc_decode keeps state in the next character, so clear that
 	bufrecv[0] = 0;
 
 	uint8_t *txp = bufxmit;
 	uint8_t *rxp = bufrecv;
 
+	// transmit the message from bufxmit to bufrecv
 	for (;;) {
 		uint8_t val  = hdlc_encode(&txp, bufxmit + 800);
 		int     flag = hdlc_decode(&rxp, bufrecv + 801, val);
@@ -25,10 +28,11 @@ int main(int argc, char *argv[]) {
 			break;
 		}
 	}
-
+	
 	int err = 0;
 	for (int i = 0; i < 800; ++i) {
-		// unflip escaped values
+		// hdlc_encode will have flipped bit 5 of escaped values in bufxmit
+		// so unflip escaped values again before comparing
 		switch (bufrecv[i]) {
 		case HDLC_ESC:
 		case HDLC_FLAG:
